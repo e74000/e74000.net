d6557bdf2faa6e68827cf4c8810be6f63b4094d8
e74000 <olimcinnes@protonmail.com>
2023-09-21 23:36:56 +0100

    Add test post program

--- programs/keebtest/main.go
+++ programs/keebtest/main.go
@@ -7,80 +7,53 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type Game struct {
-	keys       []ebiten.Key
-	mx, my     int
-	ml, mr, mm bool
-	wx, wy     float64
-}
-
-func (g *Game) Update() error {
-	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
-
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-		g.ml = true
-	} else {
-		g.ml = false
-	}
-
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
-		g.mr = true
-	} else {
-		g.mr = false
-	}
-
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonMiddle) {
-		g.mm = true
-	} else {
-		g.mm = false
-	}
-
-	g.wx, g.wy = ebiten.Wheel()
-
-	g.mx, g.my = ebiten.CursorPosition()
-
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(
-		screen,
-		fmt.Sprintf(
-			`{
-	"keys": %v,
-	"mouse_x": %d,
-	"mouse_y": %d,
-	"mouse_left_pressed":   %t,
-	"mouse_right_pressed":  %t,
-	"mouse_middle_pressed": %t,
-	"wheel_x": %f,
-	"wheel_y": %f
-}`,
-			g.keys,
-			g.mx,
-			g.my,
-			g.ml,
-			g.mr,
-			g.mm,
-			g.wx,
-			g.wy,
-		),
-	)
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 800, 600
-}
-
 func main() {
 	g := &Game{
 		keys: make([]ebiten.Key, 0),
 	}
 
-	ebiten.SetWindowSize(800, 600)
-	ebiten.SetWindowTitle("input test")
-
 	if err := ebiten.RunGame(g); err != nil {
 		panic(err)
 	}
+}
+
+type Game struct {
+	keys        []ebiten.Key
+	mouseLeft   bool
+	mouseRight  bool
+	mouseMiddle bool
+	mouseX      int
+	mouseY      int
+	wheelX      float64
+	wheelY      float64
+}
+
+func (g *Game) Update() error {
+	g.keys = inpututil.AppendPressedKeys(g.keys)
+	g.mouseLeft = ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft)
+	g.mouseRight = ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight)
+	g.mouseMiddle = ebiten.IsMouseButtonPressed(ebiten.MouseButtonMiddle)
+
+	g.mouseX, g.mouseY = ebiten.CursorPosition()
+	g.wheelX, g.wheelY = ebiten.Wheel()
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, fmt.Sprintf(
+		"KEYS: %v\nMOUSE LEFT: %t\nMOUSE RIGHT: %t\nMOUSE MIDDLE: %t\nMOUSE X: %d\nMOUSE Y: %d\nWHEEL X: %f\nWHEEL Y: %f",
+		g.keys,
+		g.mouseLeft,
+		g.mouseRight,
+		g.mouseMiddle,
+		g.mouseX,
+		g.mouseY,
+		g.wheelX,
+		g.wheelY,
+	))
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
 }
